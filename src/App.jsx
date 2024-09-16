39793e25f715c05dfeb1f89f7d3dbd17109702fd
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-09-16 02:21:45 -0300

    :bricks: Estrutura Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Api from "./api/Api.jsx";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
+import Home from "./Components/Home.jsx";
+import Login from "./Components/Login/Login.jsx";
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
       <Api />
     </div>
   );
