5ba7b082df77dc29bee58d801aefd468af15f2de
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-09-16 21:35:11 -0300

    :lipstick: Estilização do Header

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -7,14 +7,14 @@ import PhotoGet from "./endpoints/PhotoGet";
 const Api = () => {
   return (
     <div>
-      <h2>USER POST</h2>
+      {/* <h2>USER POST</h2>
       <UserPost />
       <h2>TOKEN POST</h2>
       <TokenPost />
       <h2>PHOTO POST</h2>
       <PhotoPost />
       <h2>PHOTO GET</h2>
-      <PhotoGet />
+      <PhotoGet /> */}
     </div>
   );
 };
