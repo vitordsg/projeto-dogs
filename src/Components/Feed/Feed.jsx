33d342af5dee59f59955f603463e58c521efe41e
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-10-03 21:27:23 -0300

    :wrench: Feed Modal 1

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import FeedModal from './FeedModal'
-import FeedPhotos from './FeedPhotos'
+import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
   return (
     <div>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && <FeedModal photo={modalPhoto} />}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
