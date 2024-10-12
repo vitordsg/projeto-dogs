b36929491b6c4f8a315090abc8591d5b80321a70
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-10-11 21:01:20 -0300

    :wrench: Perdeu a senha 2

--- src/api.jsx
+++ src/api.jsx
@@ -117,7 +117,20 @@ export function PASSWORD_LOST(body) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + "/api/password/reset",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     },
   };
 }
